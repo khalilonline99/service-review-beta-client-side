5dc82497428d3f950d16a39377561f8410f2fbaa
khalil <khalilonline99@gmail.com>
2022-11-28 18:17:04 +0600

    service details added

--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -3,12 +3,20 @@ import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const serviceDetails = useLoaderData();
-    const {_id, name, price, description} = serviceDetails[0];
+    const {_id, name, price, description, image} = serviceDetails[0];
     // console.log(serviceDetails[0]);
     
     return (
-        <div>
-            <h2>Service Details of: {name} </h2>
+        <div className='w-3/4 mx-auto my-16'>
+            <h2 className='font-semibold text-4xl tracking-wide text-blue-400'>Service Details of: {name} </h2>
+            <div className='mt-8'>
+                <img className='mx-auto my-5' src="https://placehold.co/600x400" alt="" />
+                <div>
+                <p className='text-left my-2'> <span className='font-bold'>Name:</span> {name} </p>
+                <p className='text-left my-2'> <span className='font-bold'>Details:</span> {description} </p>
+                <p className='font-bold text-left'> <span className='font-bold'>Price:</span> ${price} </p>
+                </div>
+            </div>
             
         </div>
     );
