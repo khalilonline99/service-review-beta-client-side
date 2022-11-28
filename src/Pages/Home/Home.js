da8bec1977fd0e787d559bedc4b1103307f2be62
khalil <khalilonline99@gmail.com>
2022-11-28 15:16:11 +0600

    added the service at home and sorting done at server

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../../Components/Hero/Hero';
+import ServiceCard from '../Services/ServiceCard';
 
 const Home = () => {
+    const [servicesHome, setServicesHome] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServicesHome(data.servicesHome))
+
+    }, []);
+
     return (
         <div>
             <Hero></Hero>
+            <div className='my-6'>
+                <h1 className='font-semibold text-2xl tracking-wide'>Check Out Our Services</h1>
+                <div className='grid grid-cols-3'>
+                {
+                    servicesHome.map(serviceHome => <ServiceCard
+                        key={serviceHome._id}
+                        service={serviceHome}
+                    ></ServiceCard>)
+                }
+                </div>
+                <button className="btn btn-primary mt-5"> <Link to='/services'>See All Services</Link> </button>
+            </div>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -6,19 +6,21 @@ const Services = () => {
 
     useEffect(() => {
         fetch('http://localhost:5000/services')
-        .then(res => res.json())
-        .then(data => setServices(data))
+            .then(res => res.json())
+            .then(data => setServices(data.services))
     }, [])
 
     return (
-        <div className='m-auto'>
-            <h2>Check All the reviews</h2>
-            {
-                services.map (service => <ServiceCard
-                key={service._id}
-                service={service}
-                ></ServiceCard>)
-            }
+        <div>
+            <h2 className='font-semibold text-4xl tracking-wide text-blue-400'>All Services:</h2>
+            <div className='grid grid-cols-3 mt-8 ml-5'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
